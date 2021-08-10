8855bdc4691acf61f8edeaa9fb471cca2dda0ef1
codeolam <frankuchenna30@gmail.com>
2021-08-10 15:36:02 +0100

    added markup build for state

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,12 +2,31 @@ import React from 'react';
 
 export default function Task({task: {id, title, state}, onArchiveTask, onPinTask}){
     return (
-        <div className="list-item">
-            <input 
-            type="text"
-            value={title}
-            readOnly={true}
-            />
+        <div className={`list-item ${state}`}>
+            <label className="checkbox">
+                <input
+                type="checkbox"
+                defaultChecked={state === 'TASK_ARCHIVED'}
+                disable={true}
+                name="checked"
+                />
+                <span className="checkbox-custom" onclick={() => onArchiveTask(id)} />
+            </label>
+            <div className="title">
+               <input 
+                type="text"
+                value={title}
+                readOnly={true}
+                /> 
+            </div>
+            <div className="actions" onClick={event => event.stopPropagation()}>
+                {state !== 'TASK_ARCHIVED' &&(
+                    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                    <a onClick={() => onPinTask(id)}>
+                        <span className={`icon-star`}/>
+                    </a>
+                )}
+            </div>
         </div>
-    )
+    );
 }
