6947c72043c5945cf128830d54536ec49fda9857
codeolam <frankuchenna30@gmail.com>
2021-08-10 16:08:56 +0100

    added types prop and installed shapshot for testing state changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Task({task: {id, title, state}, onArchiveTask, onPinTask}){
     return (
@@ -30,3 +31,14 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
         </div>
     );
 }
+
+Task.propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired
+    }),
+
+    onArchiveTask: PropTypes.func,
+    onPinTask: PropTypes.func
+};
