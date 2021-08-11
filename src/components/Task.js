27f90909523a7dff1c9a12a653f6a910e1df11ed
codeolam <frankuchenna30@gmail.com>
2021-08-11 17:59:59 +0100

    changed task backgound to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -18,6 +18,8 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
                 type="text"
                 value={title}
                 readOnly={true}
+                placeholder="Input title"
+                style={{background: 'red'}}
                 /> 
             </div>
             <div className="actions" onClick={event => event.stopPropagation()}>
